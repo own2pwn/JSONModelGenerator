60903abd7536c978d53ae161101831be59b459aa
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-07-19 00:16:09 +0300

    integrated carthage
    reading file content from the main bundle

--- JSONModelGenerator/JSONModelGenerator/ViewController.swift
+++ JSONModelGenerator/JSONModelGenerator/ViewController.swift
@@ -8,19 +8,23 @@
 
 import Cocoa
 
+enum ModelKind: String {
+    case sample
+    case gmaps = "Model"
+}
+
 final class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        testWorker()
+        testWorker(.gmaps)
     }
 
-    private func testWorker() {
-        let path = "file://" + FileManager.default.currentDirectoryPath + "/Model.json"
-        guard let url = URL(string: path),
+    private func testWorker(_ kind: ModelKind) {
+        guard let url = Bundle.main.url(forResource: kind.rawValue, withExtension: "json"),
             let content = try? Data(contentsOf: url),
             let object = try? JSONSerialization.jsonObject(with: content, options: []),
-            let json = object as? JSONObject else { return }
+            let json = object as? JSONObject else { log.warning("can't read file!"); return }
 
         let worker = Worker()
         let model = worker.generate(name: "Search", for: json)
